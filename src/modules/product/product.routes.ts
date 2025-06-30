8e9bcabc1a13f1c3d0a171f7fd5c5c9e0760b8ac
Ludwig Manansala <manansalaludwig@gmail.com>
2025-06-30 16:58:58 +0800

    Feat: create get route

--- src/modules/product/product.routes.ts
+++ src/modules/product/product.routes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createProduct } from './product.controller';
+import { createProduct, getAllProducts } from './product.controller';
 
 const router = express.Router()
 
 router.post('/', createProduct);
+router.get('/', getAllProducts);
 
 export default router
