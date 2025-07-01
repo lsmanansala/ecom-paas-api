bbb11d205934dda78103eef89a78aa0f3e367754
Azuraken05 <kaichouazura@gmail.com>
2025-07-01 16:49:16 +0800

    mali deay toh hehe

--- src/modules/product/product.routes.ts
+++ src/modules/product/product.routes.ts
@@ -1,11 +1,10 @@
 import express from 'express';
-import { createProduct, getAllProducts, deleteProduct } from './product.controller';
+import { createProduct, getAllProducts } from './product.controller';
 
 
 const router = express.Router()
 
 router.post('/', createProduct);
 router.get('/', getAllProducts);
-router.delete('/:id', deleteProduct);
 
 export default router
